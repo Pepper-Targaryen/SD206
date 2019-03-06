c3b4161f9a3af38ba7acb230919d5b3168ac06f8
Pepper <targaryenpepper@gmail.com>
2019-03-06 16:36:03 +0100

    finish tp5

---
+++ TP/TP5/bup.pl
@@ -0,0 +1,25 @@
+/*---------------------------------------------------------------*/
+/* Telecom Paristech - J-L. Dessalles 2019                       */
+/* LOGIC AND KNOWLEDGE REPRESENTATION                            */
+/*            http://teaching.dessalles.fr/LKR                   */
+/*---------------------------------------------------------------*/
+
+
+	%%%%%%%%%%%%%
+	% bottom-up recognition  %
+	%%%%%%%%%%%%%
+
+:- consult('dcg2rules.pl').     % DCG to 'rule' converter: np --> det, n. becomes rule(gn, [det, n])
+:- dcg2rules('family.pl').      % performs the conversion by asserting rule(np, [det, n])
+
+bup([s]).  % success when one gets s after a sequence of transformations
+bup(P):-
+	write(P), nl, get0(_),
+	append(Pref, Rest, P),   % P is split into three pieces 
+	append(RHS, Suff, Rest), % P = Pref + RHS + Suff
+	rule(X, RHS),	% bottom up use of rule
+	append(Pref, [X|Suff], NEWP),  % RHS is replaced by X in P:  NEWP = Pref + X + Suff
+	bup(NEWP).  % lateral recursive call
+
+go :-
+	bup([the, sister, talks, about, her, cousin]).
--- TP/TP5/family.pl
+++ TP/TP5/family.pl
@@ -8,15 +8,47 @@
 % partial elementary English grammar
 
 % --- Grammar
-s --> np, vp.
+% new grammer
+%% np(Number) --> det(Number), n(Number).
+
+%% det(singular) --> [a].
+%% det(plural) --> [many].
+%% det(_) --> [the].
+
+%% n(singular) --> [dog].
+%% n(plural) --> [dogs].
+
+%% s(Number) --> np(Number) , vp(Number).
+s --> np , vp.
 np --> det, n.		% Simple noun phrase
 np --> det, n, pp.		% Noun phrase + prepositional phrase 
-np --> [kirk].
-vp --> v.           % Verb phrase, intransitive verb
-vp --> v, np.		% Verb phrase, verb + complement:  like X
-vp --> v, pp.		% Verb phrase, verb + indirect complement : think of X 
-vp --> v, np, pp.	% Verb phrase, verb + complement + indirect complement : give X to Y 
-vp --> v, pp, pp.	% Verb phrase, verb + indirect complement + indirect complement : talk to X about Y
+% np --> [kirk].
+%% vp(Number) --> v(Number). % Verb phrase, intransitive verb
+%% vp(Number) --> v(Number), np.        % Verb phrase, verb + complement: like X
+%% vp(Number) --> v(Number), pp.        % Verb phrase, verb + indirect complement : think of X 
+%% vp(Number) --> v(Number), np, pp.    % Verb phrase, verb + complement + indirect complement : give X to Y 
+%% vp(Number) --> v(Number), pp, pp.    % Verb phrase, verb + indirect complement + indirect complement : talk to X about Y
+
+vp --> v(none).     
+vp --> v(transitive), np.     
+vp --> v(intransitive), pp.    
+vp --> v(ditransitive), np, pp. 
+vp --> v(diintransitive), pp, pp.
+
+s --> np(Sem), vp(Sem).
+np(Sem) --> det, n(Sem).
+vp(Sem) --> v(Sem, _).
+vp(Sem1) --> v(Sem1, Sem2), np(Sem2).
+
+n(sentient) --> [daughter]; [sister]; [aunt]; [sister].
+n(nonEdible) --> [door].
+n(edible) --> [apple].
+v(sentient, _) --> [sleeps].
+v(sentient, _) --> [likes].
+v(sentient, edible) --> [eats].
+v(sentient, _) --> [talks].
+v(sentient, _) --> [give].
+
 pp --> p, np.		% prepositional phrase
 
 % -- Lexicon
@@ -34,14 +66,48 @@ n --> [aunt].
 n --> [neighbour].
 n --> [cousin].
 v --> [grumbles].
-v --> [likes].
+%% v(singular) --> [hates].
+%% v(plural) --> [hate].
+%% v(singular) --> [likes].
+%% v(plural) --> [like].
 v --> [gives].
-v --> [talks].
+%% v(singular) --> [talks].
+%% v(plural) --> [talk].
 v --> [annoys].
-v --> [hates].
 v --> [cries].
+
+v(none) --> [sleeps].
+v(transitive) --> [likes].
+v(transitive) --> [eats].    % mary eats the apple
+v(none) --> [eats].            % mary eats (every day)
+v(intransitive) --> [talks].    % mary talks to peter
+v(none) --> [parle].        % mary talks (she is not mute)
+v(diintransitive) --> [parle].    % mary talks to peter about the weather
+v(ditransitive) --> [donne].    % mary gives him the book
+%% s([the, dog, talks, to, the, dog, about, the, dog], [ ]).
+%% true.
 p --> [of].
 p --> [to].
 p --> [about].
 
+np([number:sing, person:3, gender:feminine, sentience:true]) --> [mary].
+n([number:sing, person:3, gender:_, sentience:true]) --> [dog].
+n([number:plur, person:3, gender:neutral, sentience:false]) --> [apples].
+det([number:_, person:3, gender:_, sentience:_]) --> [the].
+v([subj:[number:sing, person:3, gender:_, sentience:true], event:false]) --> [thinks].
+v([subj:[number:sing, person:3, gender:_, sentience:_], event:true]) --> [falls].
 
+s --> np(FS), vp([subj:FS | _]).
+np([number, person, _, sentience]) --> det([number, person, _, sentience]), n([number, person, _, sentience]).
+vp([number, person, _, sentience]) --> v([number, person, _, sentience]). 
+
+unify(FS, FS) :- !. % Let Prolog do the job if it can
+
+unify([ Feature | R1 ], FS) :-
+    select(Feature, FS, FS1), % checks whether the Feature is in the list
+    !, % the feature has been found
+    unify(R1, FS1).
+s -->  {unify(FS1,FS2)}, np(FS1), vp([subj:FS2 | _]).
+
+go :-
+	s([mary, thinks],[]).
